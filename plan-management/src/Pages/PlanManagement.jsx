d77681d7e745614fa4586b773fa5d936686eaaac
Suhani1102 <sandhusuhani1102@gmail.com>
2022-11-04 16:44:30 +0530

    added buttons

--- plan-management/src/Pages/PlanManagement.jsx
+++ plan-management/src/Pages/PlanManagement.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Category from "../Components/Category";
+import { Box, Button, Text } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { Icon } from "@chakra-ui/react";
+// import { MdSettings } from "react-icons/md";
+import { BsPeople } from "react-icons/bs";
 
 const PlanManagement = () => {
   let planData = [
@@ -23,9 +28,22 @@ const PlanManagement = () => {
     },
   ];
   return (
-    <div>
+    <Box p="5%">
+      <Box display={"flex"} justifyContent="space-between" marginBottom="28px">
+        <Text textAlign={"left"} fontSize="32px" fontWeight={"700"}>
+          Action Plans
+        </Text>
+        <Box>
+          <Button colorScheme="messenger" variant="outline" margin={"0 10px"}>
+            <Icon as={BsPeople} w="5" h="5" mr={"5px"} /> Manage Access
+          </Button>
+          <Button colorScheme="messenger">
+            <AddIcon fontSize="12px" mr={"5px"} /> New Plan
+          </Button>
+        </Box>
+      </Box>
       <Category key={planData.id} catData={planData} />
-    </div>
+    </Box>
   );
 };
 
